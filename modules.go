4f18c4bf4a1ac672867faa868cfe5dfd5602346a
Dr. Thomas Dimakopoulos <drthomasdimakopoulos@gmail.com>
2017-08-26 19:30:53 +0300

    First Commit

---
+++ modules.go
@@ -0,0 +1,7 @@
+package main
+
+import (
+  _ "github.com/DrThomasDimakopoulos/logspout-logstash"
+  _ "github.com/gliderlabs/logspout/transports/udp"
+  _ "github.com/gliderlabs/logspout/transports/tcp"
+)
